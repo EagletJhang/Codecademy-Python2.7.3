c74c23c4ac0d5dc7ea67149730b10eff43a6c29d
Eagelt Jhang <eagletblueblue@gmail.com>
2013-07-27 13:20:47 +0800

    Update Variables and Data Types.py

--- Python-Syntax/Variables and Data Types.py
+++ Python-Syntax/Variables and Data Types.py
@@ -1,5 +1,5 @@
 my_variable=10
 #Declaring variables
 
-'""'date type: integer正整数; floats小数; booleans (which can be True or False)(大写).
-Python is case-sensitive.'""'
+'''date type: integer正整数; floats小数; booleans (which can be True or False)(大写).
+Python is case-sensitive.'''
