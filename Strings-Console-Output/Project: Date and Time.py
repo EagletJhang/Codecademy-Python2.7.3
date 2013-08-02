7e6a31cf0bd99ffe24654826038c7cace114cb4b
Eagelt Jhang <eagletblueblue@gmail.com>
2013-08-02 15:19:37 +0800

    Update Project: Date and Time.py

--- Strings-Console-Output/Project: Date and Time.py
+++ Strings-Console-Output/Project: Date and Time.py
@@ -5,3 +5,4 @@ print now.month
 print now.day
 print str(now.month)+"/"+str(now.day)+"/"+str(now.year)
 print str(now.hour)+":"+str(now.minute)+":"+str(now.second)
+print str(now.month)+"/"+str(now.day)+"/"+str(now.year)+" "+str(now.hour)+":"+str(now.minute)+":"+str(now.second)
