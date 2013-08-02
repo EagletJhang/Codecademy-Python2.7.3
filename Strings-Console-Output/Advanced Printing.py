a64d7282b28f8c4668fdad12ea60bccd299a4d73
Eagelt Jhang <eagletblueblue@gmail.com>
2013-08-02 14:32:57 +0800

    Create Advanced Printing.py

---
+++ Strings-Console-Output/Advanced Printing.py
@@ -0,0 +1,9 @@
+print "Spam "+"and "+"eggs"
+print "The value of pi is around " + str(3.14)
+
+name = raw_input("What is your name?")
+quest = raw_input("What is your quest?")
+color = raw_input("What is your favorite color?")
+
+print "Ah, so your name is %s, your quest is %s, " \
+"and your favorite color is %s." %(name, quest, color)
